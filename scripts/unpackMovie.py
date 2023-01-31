b44987c757a4e5b89aae3401f430bc4286277316
Lraty <lraty@outlook.com>
2023-01-31 18:07:36 +0800

    update unpackMovie

--- scripts/unpackMovie.py
+++ scripts/unpackMovie.py
@@ -1,13 +1,12 @@
 
 
 #need hcakey still  https://discord.com/channels/746211612981198989/1032755521624748132/1033208896438943854
-
 import os
-cridModExePath = r"D:\Game\P5RModding\unpackMovie\MOVIE_JE\MOVIE\crid_mod.exe" 
-cridParams = "-b 00234168 -a 3D2FDBA6 -v -x"
+cridModExePath = r"D:\Game\P5RModding\unpackMovie\MOVIE_JE\MOVIE\crid_mod.exe"
+cridParams = "-b 00234168 -a 3D2FDBA6 -v"  # -x for audio
 folderRoot = r"D:\Game\P5RModding\unpackMovie\MOVIE_JE\MOVIE\USM"
 
-#ffmpeg.exe -i .\MOV000.m2v -i MOV000_40534641.wav -c copy MOV000.mkv
+# ffmpeg.exe -i .\MOV000.m2v -i MOV000_40534641.wav -c copy MOV000.mkv
 ffmpeg = "ffmpeg.exe"
 
 
@@ -29,9 +28,26 @@ def cridUsm(usms):
 def mergeMovie(usms):
   # 40534641 ENG #0
   # 41534641 JPN audio #1
+  noAudUSM = []
   for usm in usms:
     usmName = usm[:-4]
-    os.system("{} -i {}.m2v -i {}_41534641.wav -c copy {}.mkv".format(ffmpeg, usmName,usmName,usmName))
+    usmFilePath = os.path.join(folderRoot, usmName)
+
+    # 优先日文音轨
+    wavJpPath = "{}_41534641 [1].wav".format(usmFilePath)
+    wavENGPath = "{}_40534641 [1].wav".format(usmFilePath)
+    wavPath = ""
+    if(os.path.exists(wavJpPath)):
+      wavPath = wavJpPath
+    elif os.path.exists(wavENGPath):
+      wavPath = wavENGPath
+    if(wavPath == ""):
+      noAudUSM.append(usm)
+      continue
+    os.system("{} -i {}.m2v -i \"{}\" -c copy {}.mkv".format(ffmpeg, usmFilePath, wavPath, usmFilePath))
+    
+  print("usm without wavs:",noAudUSM)
+
 
 def renameBins(bins):
 
@@ -43,5 +59,12 @@ if __name__ == '__main__':
   files = os.listdir(folderRoot)
   usms = gatherUSMs(files, ".USM")
   bins = gatherUSMs(files, ".bin")
+
+  # # 重命名.bin 为 hca ，给footbar2000
   # renameBins(bins)
+
+  # 解包仅视频
+  cridUsm(usms)
+
+  # 合并m2v 跟 wav
   mergeMovie(usms)
