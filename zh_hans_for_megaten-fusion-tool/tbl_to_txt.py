c7dcabebda396254bb5a6f8b7fd4550ed4e722f0
Lraty <lraty@outlook.com>
2023-02-01 21:28:05 +0800

    update tbl_tx_txt

--- zh_hans_for_megaten-fusion-tool/tbl_to_txt.py
+++ zh_hans_for_megaten-fusion-tool/tbl_to_txt.py
@@ -3,45 +3,57 @@ import struct
 import os
 from tsv_to_surrogate import readTsv, genSurroagate
 
+UNKNOW_PLACE_HOLDER = "|"
+
+#TODO 1. class ByteHandler
+# 2. separate all with 0x00
+
+
+def handleSingleByte(data, charTable, file):
+  # data: 1 byte
+  return chr(data)
+
+
+def handleDoubleByte(data, charTable, file):
+  char = UNKNOW_PLACE_HOLDER
+  nextByte = file.read(1)[0]
+  try:
+    char = charTable["{}{}".format(hex(data)[2:], hex(nextByte)[2:].zfill(2))]
+  except Exception as e:
+    pass
+  return char
+
+
+switch = {
+    1: handleSingleByte,
+    2: handleDoubleByte
+}
+
+
+def decodeTBL(charTable, bytesCount=2):
+  reversedTable = []
+  with open(filepath, 'rb') as file:
+    size = os.path.getsize(filepath)  # 获得文件大小
+
+    while file.tell() != size:
+      data0 = file.read(1)[0]
+      if(data0 == 0x00):
+        reversedTable.append(UNKNOW_PLACE_HOLDER)
+        continue
+      reversedTable.append(switch[bytesCount](data0, charTable, file))
+
+  return reversedTable
+
 
 if __name__ == '__main__':
-  tsvFilePath = "P4_hanS.tsv"
-  filepath = 'MSG_ENG.TBL'
-  unknowPlaceHolder = " "
-
+  tsvFilePath = "P5R_EFIGS.tsv"
+  filepath = 'NAME_P5R_SC.TBL'
 
   chars = readTsv(tsvFilePath)
   charTable = genSurroagate(
       chars=chars, fileName="{}.txt".format(tsvFilePath), outputFile=True)
 
-  reversedTable = []
-  with open(filepath, 'rb') as file:
-    size = os.path.getsize(filepath)  # 获得文件大小
-    
-    while file.tell() != size:
-      # DEBUG FOR ENG
-      # data = file.read(1)  # 每次输出一个字节
-      # DEBUG END
-      data0 = file.read(1)[0]
-      if(data0 == 0x00):
-        reversedTable.append(unknowPlaceHolder)
-        continue
-      data1 = file.read(1)[0]
-      char = unknowPlaceHolder
-      try:
-        char = charTable["{}{}".format(
-            hex(data0)[2:], hex(data1)[2:].zfill(2))]
-
-        # DEBUG FOR ENG
-        # char = chr(data[0])
-        # if(data[0] == 0x00):
-        #   char = "."
-        
-        # DEBUG END
-      except Exception as e:
-        pass
-      reversedTable.append(char)
-      continue
+  reversedTable = decodeTBL(charTable, bytesCount=2)
   with open("translated_{}.txt".format(filepath), "w", encoding='utf-8') as f:
     f.write("".join(reversedTable))
   # reg match \s+
