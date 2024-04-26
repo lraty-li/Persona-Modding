fc371e21fd5a2bbec28f69f0037faf2262148b63
lraty <lraty@outlook.com>
2024-04-27 01:31:14 +0800

    openai translate: bypass no sense text; continute translate from last time; remote time.sleep

--- classify_sound_file_pq2/ai_translate/translate_into_zh.py
+++ classify_sound_file_pq2/ai_translate/translate_into_zh.py
@@ -1,46 +1,95 @@
-import json
-import time
+import json,time,os
+import regex as re
 
-from openai import OpenAI
+
+import openai 
 
 from open_ai import translate
+# from sakura13b import translate
 from openai_api_ket import API_KEY
 
-msgMapPath = "D:\code\git\Persona-Modding\classify_sound_file_pq2\event_msg_map-for_ai_transl-parts.json"
+msgMapPath = "D:\code\git\Persona-Modding\classify_sound_file_pq2\msg_-ai_transl-parts.json"
 msgMap = {}
 
 
 def writeToFile(filePath, data):
     with open(filePath, "a") as file:
         file.write("{}\n".format(data))
+def readLastIndex(outPutPath):
+    line = ''
+    if(not os.path.exists(outPutPath)):
+        return ''
+    with open(outPutPath, 'r') as file:
+        lines = file.readlines()
+        if(len(lines) == 0):
+            return ''
+        line  = re.findall(r'e.*.bf.msg.*?(?= \|)', lines[-1])
+    if(len(line)<1):
+        return ''
+    return line[0]
 
+def shuildBypass(text):
+    #DEBUG
+    #text = "　"
+    #DEBUG END
+    if(text.isspace()):
+        return text.replace('\u3000','  ')
+    matchEllipsis = re.findall(r'…+。',text)
+    if(len(matchEllipsis) >0):
+        return text
+    return ''
 
 if __name__ == "__main__":
     with open(msgMapPath, "r") as msgFile:
         msgMap = json.loads(msgFile.read())
 
-    #TODO client 都由key那里创建
-    client = OpenAI(
+    client = openai.OpenAI(
         api_key=API_KEY,
-        base_url="https://free.gpt.ge/v1/",
+        base_url="https://api.v36.cm/v1/",
+        # base_url="http://localhost:5000/v1",
+
     )
 
     outputPath = msgMapPath.replace(".json", "-zh.json")
+    # outputPath = msgMapPath.replace(".json", "-zh-sakura7b.json")
+    lastIndex = readLastIndex(outputPath)
+    if(len(lastIndex) == 0):
+        # file not exist
+        pass
     writeToFile(
         outputPath,
         "========== {} ==========".format(time.asctime(time.localtime(time.time()))),
     )
     # DEBUG
-    DEBUG = True
+    DEBUG = False
     if(DEBUG):
         jp = "\u3000格言シーン"
         zh = translate(client, jp)
         print(zh)
     # DEBUG END
-    for index in msgMap:
+    msgMapKyes = msgMap.keys()
+    if(len(lastIndex) >=1):
+        msgMapKyes = list(msgMapKyes)[list(msgMapKyes).index(lastIndex)+1:]
+        pass
+    for index in msgMapKyes:
         jpMsg = msgMap[index]
-        zhMsg = translate(client, jpMsg)
+        retryThisOne = True
+        while( retryThisOne):
+            try:
+                    sShuildBypass = shuildBypass(jpMsg)
+                    # jpMsg = "オイオイ、しっかりしろよ。,ヘンな夢でもみたのか？"
+                    if(len(sShuildBypass) > 0):
+                        zhMsg = sShuildBypass
+                    else:
+                        zhMsg = translate(client, jpMsg)
+                    retryThisOne = False
+            except openai.error.InvalidRequestError as e:
+                if e.error.code == "content_filter" and e.error.innererror:
+                    retryThisOne = True
+                    content_filter_result = e.error.innererror.content_filter_result
+
+        
         line = "{} | {} | {}".format(index, jpMsg, zhMsg)
         writeToFile(outputPath, line)
         print(line)
-        time.sleep(1)
+        #time.sleep(1)
